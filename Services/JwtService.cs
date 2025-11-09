0af109b4e699d95fbb066c16afa0c98c7e0bb68b
gvesel20 <gvesel20@foi.hr>
2025-11-09 17:39:59 +0100

    Implement JWT token generation and add configuration

---
+++ Services/JwtService.cs
@@ -0,0 +1,39 @@
+﻿using IndoorLocalization.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace IndoorLocalization.Services
+{
+    public class JwtService(IConfiguration configuration)
+    {
+        public string CreateToken(User user)
+        {
+            string secretKey = configuration["Jwt:Secret"];
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            var creds = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                [
+                    new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                    new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Email, user.Email)
+                ]),
+                Expires = DateTime.UtcNow.AddMinutes(configuration.GetValue<int>("Jwt:ExpirationInMinutes")),
+                SigningCredentials = creds,
+                Issuer = configuration["Jwt:Issuer"],
+                Audience = configuration["Jwt:Audience"]
+            };
+
+            var handler = new JsonWebTokenHandler();
+
+            string token = handler.CreateToken(tokenDescriptor);
+
+            return token;
+            
+        }
+    }
+}
