64f8119777887ea7b2601d2adb2ae96845514724
gvesel20 <gvesel20@foi.hr>
2025-11-12 08:48:11 +0100

    Add DTOs for JWT refresh endpoint

---
+++ Models/DTOs/TokenResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace IndoorLocalization.Models.DTOs
+{
+    public class TokenResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
