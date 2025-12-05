13cc54d50932f3791fccd399615694089d12f428
gvesel20 <gvesel20@foi.hr>
2025-12-05 20:50:45 +0100

    Implement asset repository

---
+++ Repositories/Interfaces/IAssetRepository.cs
@@ -0,0 +1,20 @@
+﻿using IndoorLocalization.Models.Entities;
+
+namespace IndoorLocalization.Repositories.Interfaces
+{
+    public interface IAssetRepository
+    {
+        Task<IEnumerable<Asset>> GetAllAsync();
+        Task<Asset?> GetByIdAsync(long id);
+        Task<Asset?> GetByNameAsync(string name);
+        Task<bool> ExistsByNameAsync(string name);
+        Task<bool> AddAsync(Asset asset);
+        Task<bool> UpdateNameAndColorAsync(long assetId, string name, string color);
+        Task<bool> UpdateStatusAsync(long assetId, bool status);
+        Task<bool> UpdateCoordinates(long assetId, double x, double y);
+        Task<bool> UpdateFloorMap(long assetId, long? floorMapId);
+        Task<bool> DeleteAsync(Asset asset);
+
+
+    }
+}
