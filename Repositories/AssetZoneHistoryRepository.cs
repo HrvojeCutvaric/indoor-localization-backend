84d1df122f800e06541c4d77540ecf51bcf61f5e
gvesel20 <gvesel20@foi.hr>
2026-01-15 20:58:24 +0100

    Implement repository for AssetZoneHistory

---
+++ Repositories/AssetZoneHistoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using IndoorLocalization.Data;
+using IndoorLocalization.Models.Entities;
+using IndoorLocalization.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndoorLocalization.Repositories
+{
+    public class AssetZoneHistoryRepository : IAssetRepository
+    {
+        private readonly IndoorLocalizationContext _context;
+
+        public AssetZoneHistoryRepository(IndoorLocalizationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<AssetZoneHistory>> GetActiveZonesAsync(long assetId)
+        {
+            return await _context.Assetzonehistories
+                .Where(z => z.AssetId == assetId && z.ExitDateTime == null)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<AssetZoneHistory>> GetByAssetIdAsync(long assetId)
+        {
+            return await _context.Assetzonehistories
+                .Where(z => z.AssetId == assetId)
+                .OrderByDescending(z => z.EnterDateTime)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(AssetZoneHistory history)
+        {
+            _context.Assetzonehistories.Add(history);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(AssetZoneHistory history)
+        {
+            _context.Assetzonehistories.Update(history);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Repositories/Interfaces/IAssetZoneHistoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using IndoorLocalization.Models.Entities;
+
+namespace IndoorLocalization.Repositories.Interfaces
+{
+    public interface IAssetZoneHistoryRepository
+    {
+        Task<IEnumerable<AssetZoneHistory>> GetActiveZonesAsync(long assetId);
+        Task AddAsync(AssetZoneHistory history);
+        Task UpdateAsync(AssetZoneHistory history);
+        Task<IEnumerable<AssetZoneHistory>> GetByAssetIdAsync(long assetId);
+    }
+}
