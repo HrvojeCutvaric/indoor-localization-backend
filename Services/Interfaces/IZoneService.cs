aa3941b23893271f11084e863957ec4b0753c67e
gvesel20 <gvesel20@foi.hr>
2026-01-15 19:17:15 +0100

    Implement zone service

---
+++ Services/Interfaces/IZoneService.cs
@@ -0,0 +1,14 @@
+﻿using IndoorLocalization.Models.DTOs;
+
+namespace IndoorLocalization.Services.Interfaces
+{
+    public interface IZoneService
+    {
+        Task<IEnumerable<ZoneResponseDto>> GetAllAsync();
+        Task<IEnumerable<ZoneResponseDto>> GetByFloorMapAsync(long floorMapId);
+        Task<ZoneResponseDto?> GetByIdAsync(long id);
+        Task<ZoneResponseDto> CreateAsync(ZoneCreateRequestDto dto);
+        Task<bool> UpdateAsync(long id, ZoneUpdateRequestDto dto);
+        Task<bool> DeleteAsync(long id);
+    }
+}
