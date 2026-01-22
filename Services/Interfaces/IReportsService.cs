1095bfe5b851fd98088bf726649b17732d60ec2e
gvesel20 <gvesel20@foi.hr>
2026-01-22 19:18:44 +0100

    Add semicolon :)

--- Services/Interfaces/IReportsService.cs
+++ Services/Interfaces/IReportsService.cs
@@ -6,6 +6,6 @@ namespace IndoorLocalization.Services.Interfaces
     {
         Task<IEnumerable<AssetPositionHistoryResponseDto>> GetPositionHistoryByRangeAsync(long assetId, DateTime from, DateTime to);
         Task<IEnumerable<AssetPositionHistoryResponseDto>> GetPositionHistoryByFloorMapAsync(long floorMapId, DateTime from, DateTime to);
-        Task<IEnumerable<AssetZoneHistoryResponseDto>> GetZoneRetentionAsync(long? assetId, long? zoneId, DateTime? from, DateTime? to)
+        Task<IEnumerable<AssetZoneHistoryResponseDto>> GetZoneRetentionAsync(long? assetId, long? zoneId, DateTime? from, DateTime? to);
     }
 }
